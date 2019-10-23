1a0613a06865907c4c1b00fae59b66345a7d9ef0
Sergei Mamykin <mamykin.sergei@gmail.com>
2019-10-23 20:31:31 +0300

    добавил реакт

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,5 @@
 //main styles
 import '../style/main.scss';
+
+import React from 'react';
+import ReactDOM from  'react-dom';
--- src/js/second.js
+++
@@ -1,5 +0,0 @@
-setTimeout(() => console.log('hi'), 3000);
-
-let n  = document.createElement('div');
-n.innerHTML = require('./templates/new.twig')();
-document.body.appendChild(n);
