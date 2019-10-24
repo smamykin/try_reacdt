d8795a9871c7e677ebe89141115306fb2b127024
Mamykin Sergei <mamykin.sergei@gmail.com>
2019-10-24 22:12:40 +0300

    task 2

--- src/js/main.js
+++ src/js/main.js
@@ -4,25 +4,28 @@ import '../style/main.scss';
 import React from 'react';
 import ReactDOM from  'react-dom';
 
-class Article extends React.Component
+class App extends React.Component
 {
+    constructor(props){
+        super(props);
+        this.state = {text: ''};
+    }
+
+    onchange(evt){
+        this.setState({text: evt.target.value});
+    }
+
     render(){
-        return (<div className={'article'}>
-            <h1 className={'article__title'}>{this.props.title}</h1>
-            <div className={'article__author'}>{this.props.author}</div>
-            <div className={'article__text'}>{this.props.text}</div>
-        </div>);
+        return (
+            <div>
+                <input type={'text'} placeholder={'Whom to say hello?'} onChange={this.onchange.bind(this)}/>
+                <div>Hello, {this.state.text ? this.state.text : 'stranger' }!</div>
+            </div>
+        );
     }
 }
 
-let article = {
-    title: 'Melted carrots can be made dark by tossing with peanut sauce.',
-    author: 'Jhon Wick',
-    text: 'Try pressing cake flavored with maple syrup, brushed with cinnamon. Try seasoning the red wine pickles with delicious remoulade and crême fraîche, sautéed.' +
-        'Place the ground beef in a bucket, and marinate thoroughly with hot adobo sauce. Everyone loves the viscosity of bagel ricotta flavord with sun-dried za\'atar.'
-};
-
 ReactDOM.render(
-    <Article title={article.title} author={article.author} text={article.text}/>,
+    <App/>,
     document.getElementById('app')
 );
