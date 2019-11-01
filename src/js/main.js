c2ae138e67f41e5fdcbf83722ac140f593e44c11
Sergei Mamykin <mamykin.sergei@gmail.com>
2019-11-01 19:31:11 +0300

    Задача 2.4 - перенес верстку в компонент

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NotesApp from './parts/NotesApp'
 import Timer from "./parts/Timer";
+import ToDoApp from "./parts/ToDoApp";
 
 
-// ReactDOM.render(
-//     {<ToDoApp/>,}
-    // document.getElementById('mount-point')
-// );
+ReactDOM.render(
+    <ToDoApp/>,
+    document.getElementById('mount-point')
+);
--- src/js/parts/ToDoApp.js
+++ src/js/parts/ToDoApp.js
@@ -1,9 +1,47 @@
-
 import React from 'react';
+import classNames from 'classnames';
 
-export default class ToDoApp extends  React.Component
-{
-    render(){
+export default class ToDoApp extends React.Component {
+    getTasks() {
+        return [
+            this.createTask(),
+            this.createTask(),
+            this.createTask(),
+            this.createTask(),
+            this.createTask()
+        ];
+    }
 
+    createTask() {
+        return {
+            text: 'When breaking large sauerkraut',
+            isDone: false
+        }
+    }
+
+    render() {
+        return (
+            <div className="todo">
+                <div className="todo__header"><h2 className="todo__title">What do you need to do?</h2></div>
+                <ul className="todo__task-list">
+                    {this.getTasks().map((task) => {
+                        return (
+                            <li className={classNames('task-item', {checked: task.isDone})}>
+                                <div className="task-item__check"/>
+                                <div className="task-item__text ">
+                                    {task.text}
+                                </div>
+                                <div className="task-item__delete"/>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <ul className="todo__filter-control">
+                    <li className="todo__filter-item">All</li>
+                    <li className="todo__filter-item">New</li>
+                    <li className="todo__filter-item">Completed</li>
+                </ul>
+            </div>
+        );
     }
 }
