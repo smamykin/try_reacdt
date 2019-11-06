4a14ae579c3d80edfe4e948e9d0cdb606533a896
Mamykin Sergei <mamykin.sergei@gmail.com>
2019-11-06 21:41:02 +0300

    task 3.3 - здесь буду создавать компоненты

---
+++ src/js/parts/FinanceApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {CssBaseline} from "@material-ui/core";
+
+export default class FinanceApp extends React.Component
+{
+    render(){
+        return (
+            <React.Fragment>
+                <CssBaseline/>
+            </React.Fragment>
+        );
+    }
+}
