35b14f1abc9f7af7e1fb1108fc652edf004a45e3
Mamykin Sergei <mamykin.sergei@gmail.com>
2019-11-10 18:44:56 +0300

    task 4.2 - ссылка назад

--- src/js/parts/ArticlesApp/Article.js
+++ src/js/parts/ArticlesApp/Article.js
@@ -9,6 +9,9 @@ export default class Article extends React.Component{
                     <div className="article-page__body">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur at atque, consectetur, doloribus earum error est excepturi incidunt laboriosam maxime neque nobis quasi reprehenderit, tempora vero? Assumenda fuga ut voluptatum?
                     </div>
+                    <div className="article-page__back-block">
+                        <a href="#" className="article-page__back-block">back</a>
+                    </div>
                 </div>
             </div>
         );
