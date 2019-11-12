3c66e3dbfa257c2fc7c5ff3fe485c1472227d2f3
Sergei Mamykin <mamykin.sergei@gmail.com>
2019-11-12 19:19:00 +0300

    task 4.3 - ссылка на отдельный товар

--- src/js/parts/IM/GoodsList.js
+++ src/js/parts/IM/GoodsList.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
 
 export default class GoodsList extends React.Component
 {
     render() {
         return (
             <div className={'catalog'}>
+
                 <div className="catalog-search">
                     <input type="text" className="catalog-search__input" placeholder={'Search'}/>
                 </div>
@@ -16,7 +18,7 @@ export default class GoodsList extends React.Component
                         };
                        return (
                            <div key={v.id} className="catalog-list__item catalog-card" style={style}>
-                               <div className="catalog-card__title">{v.name}</div>
+                               <Link to={'/goods/' + v.id} className="catalog-card__title">{v.name} </Link>
                                <div className="catalog-card__buy-button" onClick={this.props.buy(v)}>Купит</div>
                            </div>
                        );
