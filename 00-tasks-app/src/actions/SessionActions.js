ace6d0e61498bbfcc4191b47a43864cd3e6b7350
Sergei Mamykin <mamykin.sergei@gmail.com>
2019-11-14 16:26:04 +0300

    task 5 - чтобы можно было выполнять задания из Резюме

---
+++ 00-tasks-app/src/actions/SessionActions.js
@@ -0,0 +1,29 @@
+import AppDispatcher from '../dispatcher/AppDispatcher';
+import AppConstants from '../constants/AppConstants';
+
+import api from '../api';
+
+const SessionActions = {
+    authorize(immediate = false, callback) {
+        api.authorize({ immediate })
+        .then(() => {
+            AppDispatcher.dispatch({
+                type: AppConstants.SESSION_AUTHORIZE_SUCCESS
+            });
+
+            if (callback) callback();
+        })
+        .catch((err) => {
+
+            console.info('hi');
+            AppDispatcher.dispatch({
+                type: AppConstants.SESSION_AUTHORIZE_FAIL,
+                error: err
+            });
+
+            if (callback) callback();
+        });
+    }
+};
+
+export default SessionActions;
