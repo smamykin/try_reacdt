01f9e1fe851331727fe24e5176a2c7541dd1370a
Sergei Mamykin <mamykin.sergei@gmail.com>
2019-11-15 18:19:07 +0300

    task 5-3 - чтобы название изменялось

--- 00-tasks-app/src/components/TasksPage.jsx
+++ 00-tasks-app/src/components/TasksPage.jsx
@@ -33,12 +33,12 @@ const TasksPage = React.createClass({
 
     componentDidMount() {
         TasksStore.addChangeListener(this._onChange);
-        TaskListsActions.loadTaskList(this.props.params.id);
     },
 
     componentWillReceiveProps(nextProps) {
         if (this.props.params.id !== nextProps.params.id) {
             TasksActions.loadTasks(nextProps.params.id);
+            TaskListsActions.loadTaskList(this.props.params.id);
         }
     },
 
